2190496d669872b5497e5b542d2f717374fbd12c
Viktoria Zaharova <zaharova181114@gmail.com>
2025-02-25 09:38:41 +0200

    index mobile

--- js/common.js
+++ js/common.js
@@ -134,190 +134,183 @@ $('.btn-close-poppup').on('click', function (e) {
 
 
 // datepicker
-// $.datepicker.setDefaults({
-// 	closeText: 'Закрыть',
-// 	prevText: '',
-// 	currentText: 'Сегодня',
-// 	monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-// 		'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-// 	monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
-// 		'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
-// 	dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-// 	dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
-// 	dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-// 	weekHeader: 'Не',
-// 	dateFormat: 'dd.mm.yy',
-// 	firstDay: 1,
-// 	isRTL: false,
-// 	showMonthAfterYear: false,
-// 	yearSuffix: ''
-// });
+$.datepicker.setDefaults({
+	closeText: 'Закрыть',
+	prevText: '',
+	currentText: 'Сегодня',
+	monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+		'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+	monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+		'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+	dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+	dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+	dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+	weekHeader: 'Не',
+	dateFormat: 'dd.mm.yy',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''
+});
 
-// (function ($) {
-// 	function compareDates(startDate, endDate, format) {
-// 		var temp, dateStart, dateEnd;
-// 		try {
-// 			dateStart = $.datepicker.parseDate(format, startDate);
-// 			dateEnd = $.datepicker.parseDate(format, endDate);
-// 			if (dateEnd < dateStart) {
-// 				temp = startDate;
-// 				startDate = endDate;
-// 				endDate = temp;
-// 			}
-// 		} catch (ex) { }
-// 		return { start: startDate, end: endDate };
-// 	}
+(function ($) {
+	function compareDates(startDate, endDate, format) {
+		var temp, dateStart, dateEnd;
+		try {
+			dateStart = $.datepicker.parseDate(format, startDate);
+			dateEnd = $.datepicker.parseDate(format, endDate);
+			if (dateEnd < dateStart) {
+				temp = startDate;
+				startDate = endDate;
+				endDate = temp;
+			}
+		} catch (ex) { }
+		return { start: startDate, end: endDate };
+	}
 
-// 	$.fn.dateRangePicker = function (options) {
-// 		options = $.extend(
-// 			{
-// 				changeMonth: false,
-// 				changeYear: false,
-// 				numberOfMonths: 1,
-// 				rangeSeparator: " - ",
-// 				useHiddenAltFields: false
-// 			},
-// 			options || {}
-// 		);
+	$.fn.dateRangePicker = function (options) {
+		options = $.extend(
+			{
+				changeMonth: false,
+				changeYear: false,
+				numberOfMonths: 1,
+				rangeSeparator: " - ",
+				useHiddenAltFields: false
+			},
+			options || {}
+		);
 
-// 		var myDateRangeTarget = $(this);
-// 		var onSelect = options.onSelect || $.noop;
-// 		var onClose = options.onClose || $.noop;
-// 		var beforeShow = options.beforeShow || $.noop;
-// 		var beforeShowDay = options.beforeShowDay;
-// 		var lastDateRange;
+		var myDateRangeTarget = $(this);
+		var onSelect = options.onSelect || $.noop;
+		var onClose = options.onClose || $.noop;
+		var beforeShow = options.beforeShow || $.noop;
+		var beforeShowDay = options.beforeShowDay;
+		var lastDateRange;
 
-// 		function storePreviousDateRange(dateText, dateFormat) {
-// 			var start, end;
-// 			dateText = dateText.split(options.rangeSeparator);
-// 			if (dateText.length > 0) {
-// 				start = $.datepicker.parseDate(dateFormat, dateText[0]);
-// 				if (dateText.length > 1) {
-// 					end = $.datepicker.parseDate(dateFormat, dateText[1]);
-// 				}
-// 				lastDateRange = { start: start, end: end };
-// 			} else {
-// 				lastDateRange = null;
-// 			}
-// 		}
+		function storePreviousDateRange(dateText, dateFormat) {
+			var start, end;
+			dateText = dateText.split(options.rangeSeparator);
+			if (dateText.length > 0) {
+				start = $.datepicker.parseDate(dateFormat, dateText[0]);
+				if (dateText.length > 1) {
+					end = $.datepicker.parseDate(dateFormat, dateText[1]);
+				}
+				lastDateRange = { start: start, end: end };
+			} else {
+				lastDateRange = null;
+			}
+		}
 
-// 		options.beforeShow = function (input, inst) {
-// 			var dateFormat = myDateRangeTarget.datepicker("option", "dateFormat");
-// 			storePreviousDateRange($(input).val(), dateFormat);
-// 			beforeShow.apply(myDateRangeTarget, arguments);
-// 		};
+		options.beforeShow = function (input, inst) {
+			var dateFormat = myDateRangeTarget.datepicker("option", "dateFormat");
+			storePreviousDateRange($(input).val(), dateFormat);
+			beforeShow.apply(myDateRangeTarget, arguments);
+		};
 
-// 		options.beforeShowDay = function (date) {
-// 			var out = [true, ""],
-// 				extraOut;
-// 			if (lastDateRange && lastDateRange.start <= date) {
-// 				if (lastDateRange.end && date <= lastDateRange.end) {
-// 					out[1] = "ui-datepicker-range";
-// 				}
-// 			}
+		options.beforeShowDay = function (date) {
+			var out = [true, ""],
+				extraOut;
+			if (lastDateRange && lastDateRange.start <= date) {
+				if (lastDateRange.end && date <= lastDateRange.end) {
+					out[1] = "ui-datepicker-range";
+				}
+			}
 
-// 			if (beforeShowDay) {
-// 				extraOut = beforeShowDay.apply(myDateRangeTarget, arguments);
-// 				out[0] = out[0] && extraOut[0];
-// 				out[1] = out[1] + " " + extraOut[1];
-// 				out[2] = extraOut[2];
-// 			}
-// 			return out;
-// 		};
+			if (beforeShowDay) {
+				extraOut = beforeShowDay.apply(myDateRangeTarget, arguments);
+				out[0] = out[0] && extraOut[0];
+				out[1] = out[1] + " " + extraOut[1];
+				out[2] = extraOut[2];
+			}
+			return out;
+		};
 
-// 		options.onSelect = function (dateText, inst) {
-// 			var textStart;
-// 			if (!inst.rangeStart) {
-// 				inst.inline = true;
-// 				inst.rangeStart = dateText;
-// 			} else {
-// 				inst.inline = false;
-// 				textStart = inst.rangeStart;
-// 				if (textStart !== dateText) {
-// 					var dateFormat = myDateRangeTarget.datepicker("option", "dateFormat");
-// 					var dateRange = compareDates(textStart, dateText, dateFormat);
-// 					myDateRangeTarget.val(
-// 						dateRange.start + options.rangeSeparator + dateRange.end
-// 					);
-// 					inst.rangeStart = null;
-// 					if (options.useHiddenAltFields) {
-// 						var myToField = myDateRangeTarget.attr("data-to-field");
-// 						var myFromField = myDateRangeTarget.attr("data-from-field");
-// 						$("#" + myFromField).val(dateRange.start);
-// 						$("#" + myToField).val(dateRange.end);
-// 					}
-// 				}
-// 			}
-// 			onSelect.apply(myDateRangeTarget, arguments);
-// 		};
+		options.onSelect = function (dateText, inst) {
+			var textStart;
+			if (!inst.rangeStart) {
+				inst.inline = true;
+				inst.rangeStart = dateText;
+			} else {
+				inst.inline = false;
+				textStart = inst.rangeStart;
+				if (textStart !== dateText) {
+					var dateFormat = myDateRangeTarget.datepicker("option", "dateFormat");
+					var dateRange = compareDates(textStart, dateText, dateFormat);
+					myDateRangeTarget.val(
+						dateRange.start + options.rangeSeparator + dateRange.end
+					);
+					inst.rangeStart = null;
+					if (options.useHiddenAltFields) {
+						var myToField = myDateRangeTarget.attr("data-to-field");
+						var myFromField = myDateRangeTarget.attr("data-from-field");
+						$("#" + myFromField).val(dateRange.start);
+						$("#" + myToField).val(dateRange.end);
+					}
+				}
+			}
+			onSelect.apply(myDateRangeTarget, arguments);
+		};
 
-// 		options.onClose = function (dateText, inst) {
-// 			inst.rangeStart = null;
-// 			inst.inline = false;
-// 			onClose.apply(myDateRangeTarget, arguments);
-// 		};
+		options.onClose = function (dateText, inst) {
+			inst.rangeStart = null;
+			inst.inline = false;
+			onClose.apply(myDateRangeTarget, arguments);
+		};
 
-// 		return this.each(function () {
-// 			if (myDateRangeTarget.is("input")) {
-// 				myDateRangeTarget.datepicker(options);
-// 			}
-// 			myDateRangeTarget.wrap('<div class="dateRangeWrapper"></div>');
-// 		});
-// 	};
-// })(jQuery);
+		return this.each(function () {
+			if (myDateRangeTarget.is("input")) {
+				myDateRangeTarget.datepicker(options);
+			}
+			myDateRangeTarget.wrap('<div class="dateRangeWrapper"></div>');
+		});
+	};
+})(jQuery);
 
-// $(document).ready(function () {
-// 	$(".datepicker-2").dateRangePicker({
-// 		showOn: "focus",
-// 		rangeSeparator: " - ",
-// 		dateFormat: "dd.mm.yy",
-// 		useHiddenAltFields: true,
-// 		constrainInput: true
-// 	});
+$(document).ready(function () {
+	$(".datepicker-2").dateRangePicker({
+		showOn: "focus",
+		rangeSeparator: " - ",
+		dateFormat: "dd.mm.yy",
+		useHiddenAltFields: true,
+		constrainInput: true
+	});
 
-// 	$(".datepicker-2-1").dateRangePicker({
-// 		showOn: "focus",
-// 		rangeSeparator: " - ",
-// 		dateFormat: "dd.mm.yy",
-// 		useHiddenAltFields: true,
-// 		constrainInput: true
-// 	});
+	$(".datepicker-2-1").dateRangePicker({
+		showOn: "focus",
+		rangeSeparator: " - ",
+		dateFormat: "dd.mm.yy",
+		useHiddenAltFields: true,
+		constrainInput: true
+	});
 
-// 	$(".datepicker-2-2").datepicker({
-// 		showOn: "focus",
-// 		// rangeSeparator: " - ",
-// 		dateFormat: "dd.mm.yy",
-// 		useHiddenAltFields: true,
-// 		constrainInput: true
-// 	});
-// });
+	$(".datepicker-2-2").datepicker({
+		showOn: "focus",
+		// rangeSeparator: " - ",
+		dateFormat: "dd.mm.yy",
+		useHiddenAltFields: true,
+		constrainInput: true
+	});
+});
 
 
 // file input
-// $.fileup({
-// 	url: window.location.pathname + window.location.search,
-// 	inputID: 'upload-demo',
-// 	queueID: 'upload-demo-queue',
-// 	dropzoneID: '',
-// 	files: [],
-// 	fieldName: 'filedata',
-// 	extraFields: {},
-// 	sizeLimit: 0,
-// 	filesLimit: 0,
-// 	method: 'post',
-// 	// timeout: null,
-// 	autostart: false,
-// });
+$.fileup({
+	url: window.location.pathname + window.location.search,
+	inputID: 'upload-demo',
+	queueID: 'upload-demo-queue',
+	dropzoneID: '',
+	files: [],
+	fieldName: 'filedata',
+	extraFields: {},
+	sizeLimit: 0,
+	filesLimit: 0,
+	method: 'post',
+	// timeout: null,
+	autostart: false,
+});
 
 // tabs
-// $(function () {
-// 	$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
-// 		$(this)
-// 			.addClass('active').siblings().removeClass('active')
-// 			.parents('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-// 	});
 
-// });
 
 $(document).ready(function ($) {
 	$('.tabs__caption li a').click(function (e) {
